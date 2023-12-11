110356414db43ae4c6cb2a9a0174e1b38be56b97
Mohamed <moh.medh.tem1@gmail.com>
2023-12-11 08:29:13 +0200

    Section Done!

---
+++ Section 2 - TypeScript Basics Basic Types/app.js
@@ -0,0 +1,12 @@
+var userInput;
+var userName;
+userInput = 5;
+userInput = 'Max';
+if (typeof userInput === 'string') {
+    userName = userInput;
+}
+function generateError(message, code) {
+    throw { message: message, errorCode: code };
+    // while (true) {}
+}
+generateError('An error occurred!', 500);
